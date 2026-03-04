3b33690e6f09f3664e72173805b2ce627cbfc523
AChar770 <amandatcharles@yahoo.com>
2026-03-03 20:32:29 -0500

    get that sends a single employee by id

--- app.js
+++ app.js
@@ -16,11 +16,14 @@ app.get("/employees/random", (req, res) => {
   res.json(employees[randomIndex]);
   });
 
+app.get("/employees/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const employee = employees.find((e) => e.id === id);
 
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
 
-
-//
-
-app.get("/", (req, res) => {
-  res.send(`"Hello employee!"`);
+  res.json(employee);
 });
+
